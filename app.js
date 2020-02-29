6773b33cbc636514193c32ef779463a2fd9e6470
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2020-02-29 10:48:06 +0530

    ha to app

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ha = express();
-ha.use(bodyParser.urlencoded({extended: true}));
-ha.use(express.static("public"));
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-ha.get("/", function(req,res){
+app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
-ha.post("/", function(req,res){
+app.post("/", function(req,res){
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1+num2;
@@ -21,7 +21,7 @@ if(port == null || port == "") {
   port = 3000;
 }
 
-ha.listen(port, function() {
+app.listen(port, function() {
   console.log("Server started Successfully");
 });
 

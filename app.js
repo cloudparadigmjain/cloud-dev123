916fbf752553dfd25399c52c3eb86e00f5292189
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2020-05-08 20:37:31 +0530

    Multiple Markers for Admin Portal

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ var a2="jatin";
 var counter="jatin";
 const app = express();
 
+var PubNub = require("pubnub");
+
+var pubnub = new PubNub({
+    subscribeKey: "sub-c-db832570-885f-11ea-a961-f6bfeb2ef611",
+    publishKey: "pub-c-4fe2d8f8-74ed-4d6a-93c4-149e68832d07",
+    uuid: "myUniqueUUIDServer",
+    secretKey: "sec-c-YWM1MzAyMGYtODljNi00MGJmLWFhM2MtNWVkNjBiNGI5NzEy"
+});
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -198,6 +207,10 @@ req.login(user, function(err) {
       });
   }
 });
+});
+
+app.get("/test", function(req,res) {
+  res.render("drivers/test");
 });
 
 app.get("/adminPortal/:username/driversmap", function(req,res) {
@@ -383,6 +396,35 @@ setTimeout(delay,2000);
       }
     });
   });
+
+  pubnub.channelGroups.addChannels(
+    {
+        channels: ['ch1', 'ch2','ch3'],
+        channelGroup: "myChannelGroup"
+    }, 
+    function(status) {
+        if (status.error) {
+            console.log("operation failed w/ status: ", status);
+        } else {
+            console.log("operation done!", status);
+        }
+    }
+  );
+  
+  
+  pubnub.addListener({
+      message: function(m,ch) {
+          // handle message
+          console.log("message recieved:",JSON.stringify(m));
+      }
+  });
+  
+  pubnub.subscribe({
+      channelGroups: ['myChannelGroup'],
+  }, function (err) {
+      console.log(err);
+  });
+  
 
 
   let port = process.env.PORT;
